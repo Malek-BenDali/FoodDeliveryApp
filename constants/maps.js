8f870fdba12e4acbcf47ae06ca09cef6c9a0b94a
Malek Ben Dali <bendali.malekk@gmail.com>
2020-12-24 03:02:22 +0100

    added maps

--- constants/maps.js
+++ constants/maps.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY = "AIzaSyD_lMXvGFGsiO2qhvX7Vs49r-sdICRtrAQ"
+const GOOGLE_API_KEY = "AIzaSyCHOHo77ma7TNbLJchJxw7pE13vUpIVhXw"
 
 export default GOOGLE_API_KEY
--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import MapView, {PROVIDER_GOOGLE, Marker}  from 'react-native-maps'
+import {COLORS, FONTS, icons, SIZES, GOOGLE_API_KEY} from "../constants"
 
 const OrderDelivery = () => {
+
+    const [restaurant, setrestaurant] = useState(initialState)
+
+    const mapfood = () =>{
+        return(
+            <View style={{flex:1}}>
+                <MapView
+                    style={{flex : 1}}
+                >
+
+                </MapView>
+            </View>
+        )
+    }
+
     return (
-        <View>
-            <Text>OrderDelivery</Text>
+        <View style={{flex : 1}}>
+            {mapfood()}
         </View>
     )
 }
